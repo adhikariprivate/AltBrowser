05e7f79e772c6bc75a72246ce2d3921043bc6f3f
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-08 19:37:59 +0545

    Rclone mount try fix 1 for commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ rclone config create 'CLOUD_STORAGE' 'mega' 'user' $MEGA_USER 'pass' $MEGA_PASS
 
 mkdir mountpoint
 
-rclone mount CLOUD_STORAGE:/ mountpoint --allow-non-empty --vfs-cache-mode full --daemon
+rclone mount CLOUD_STORAGE:/ mountpoint --daemon --vfs-cache-mode full 
 
 # Run the filebrowser
 ./filebrowser --address 0.0.0.0 --port $PORT --username $ADMIN_USER --password $ADMIN_PASSWORD
