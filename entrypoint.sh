9829478722c95d2911e533082244cff2bf53ad65
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-08 22:42:24 +0545

    Added missing elif

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 if [ $NO_OF_UNION_BASE == "one" ]; then
     rclone config create 'U0' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     
-if [ $NO_OF_UNION_BASE == "two" ]; then
+elif [ $NO_OF_UNION_BASE == "two" ]; then
     rclone config create 'U1' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     rclone config create 'U2' 'mega' 'user' $MEGA_USER_2 'pass' $MEGA_PASS_2
     rclone config create 'U0' 'union' upstreams "U1:/ U2:/"
