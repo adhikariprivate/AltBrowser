4ab23b3fa5e3b3fd338b22ddb097907204430d29
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-08 19:17:45 +0545

    Added Rclone Mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
+# Configure Rclone
+rclone config create 'CLOUD_STORAGE' 'mega' 'user' $MEGA_USER 'pass' $MEGA_PASS
+
+mkdir mountpoint
+
+rclone mount CLOUD_STORAGE:/ mountpoint --allow-non-empty --vfs-cache-mode full --daemon
+
 # Run the filebrowser
 ./filebrowser --address 0.0.0.0:$PORT --username $ADMIN_USER --password $ADMIN_PASSWORD
