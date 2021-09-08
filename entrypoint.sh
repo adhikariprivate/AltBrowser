e9aa63bddff2b08b6f947e2ddec051765e99f849
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-08 22:48:43 +0545

    Removed space from operators

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,26 @@
 # Configure Rclone
-if [ $NO_OF_UNION_BASE == "one" ]; then
+if [ $NO_OF_UNION_BASE=="one" ]; then
     rclone config create 'U0' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     
-elif [ $NO_OF_UNION_BASE == "two" ]; then
+elif [ $NO_OF_UNION_BASE=="two" ]; then
     rclone config create 'U1' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     rclone config create 'U2' 'mega' 'user' $MEGA_USER_2 'pass' $MEGA_PASS_2
     rclone config create 'U0' 'union' upstreams "U1:/ U2:/"
 
-elif [ $NO_OF_UNION_BASE == "three"]; then
+elif [ $NO_OF_UNION_BASE=="three"]; then
     rclone config create 'U1' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     rclone config create 'U2' 'mega' 'user' $MEGA_USER_2 'pass' $MEGA_PASS_2
     rclone config create 'U3' 'mega' 'user' $MEGA_USER_3 'pass' $MEGA_PASS_3
     rclone config create 'U0' 'union' upstreams "U1:/ U2:/ U3:/"
 
-elif [ $NO_OF_UNION_BASE == "four"]; then
+elif [ $NO_OF_UNION_BASE=="four"]; then
     rclone config create 'U1' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     rclone config create 'U2' 'mega' 'user' $MEGA_USER_2 'pass' $MEGA_PASS_2
     rclone config create 'U3' 'mega' 'user' $MEGA_USER_3 'pass' $MEGA_PASS_3
     rclone config create 'U4' 'mega' 'user' $MEGA_USER_4 'pass' $MEGA_PASS_4
     rclone config create 'U0' 'union' upstreams "U1:/ U2:/ U3:/ U4:/"
 
-elif [ $NO_OF_UNION_BASE == "five"]; then
+elif [ $NO_OF_UNION_BASE=="five"]; then
     rclone config create 'U1' 'mega' 'user' $MEGA_USER_1 'pass' $MEGA_PASS_1
     rclone config create 'U2' 'mega' 'user' $MEGA_USER_2 'pass' $MEGA_PASS_2
     rclone config create 'U3' 'mega' 'user' $MEGA_USER_3 'pass' $MEGA_PASS_3
