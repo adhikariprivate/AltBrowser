63be5af0dd7d5e1e61cab77c69d8f18f3f85e5fa
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-21 18:21:05 +0545

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ mkdir mountpoint
 rclone mount CLOUD_STORAGE:/ mountpoint --daemon --vfs-cache-mode full 
 
 # Run the filebrowser
-./filebrowser --address 0.0.0.0 --port $PORT -r mountpoint
+./filebrowser --address 0.0.0.0 --port $PORT -r /
