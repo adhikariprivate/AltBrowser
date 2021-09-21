98b2b39303bc41ead5238f29ec46ecbd593341f9
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-09-21 23:54:12 +0545

    Echo modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo $RCLONE_CONF > ~/.config/rclone/rclone.conf
+echo -e "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 
 rclone serve webdav $SERVICE_NAME:/ --addr 0.0.0.0:$PORT --vfs-cache-mode full --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --user $WEBDAV_USER --pass $WEBDAV_PASS
