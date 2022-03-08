f26e74032194a244cb1225cf07e5ad53ddbe667f
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2022-03-08 08:33:49 +0545

    Added $PATH in Environment Variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "$RCLONE_CONF" > ./rclone.conf
 
-rclone serve webdav $SERVICE_NAME:/ --addr 0.0.0.0:$PORT --vfs-cache-mode full --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --user $WEBDAV_USER --pass $WEBDAV_PASS --config ./rclone.conf
+rclone serve webdav $SERVICE_NAME:$PATH --addr 0.0.0.0:$PORT --vfs-cache-mode full --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --user $WEBDAV_USER --pass $WEBDAV_PASS --config ./rclone.conf
